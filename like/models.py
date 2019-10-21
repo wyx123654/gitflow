763d40f43bc8dc281d9a8c0113f6100c9fc25815
wangyuxi <961871881@qq.com>
2019-10-21 23:32:08 +0800

    tianjia Model

--- like/models.py
+++ like/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Like(models,Model):
+    uid = models.IntegerField()
+    name = models.CharField()
+    other_id = models.IntegerField()
+    other_name = models.CharField()
